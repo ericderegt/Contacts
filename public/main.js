2da2f9a95ec799172dc5055292056e5e2e3342a0
Eric deRegt <ericderegt@gmail.com>
2015-05-02 11:50:17 -0400

    some css

--- public/main.js
+++ public/main.js
@@ -1,3 +1,12 @@
-$('.ui.dropdown')
-  .dropdown()
-;
+$(document).ready(function(){
+
+  $('.ui.dropdown').click(function(){
+    $(this).dropdown('show');
+  });
+
+  $('a.item').click(function(){
+    $('.item').removeClass('active');
+    $(this).addClass('active');
+  });
+
+});
